3d40bfbaa6a22041b80d687a65a5e71ccb42aa12
mohanadefm <72825467+mohanadefm@users.noreply.github.com>
2022-05-16 11:41:21 +0300

    Finished Project

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-// window.console.log("Hey");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Show New Quotes
+function newQuote() {
+  showLoadingSpinner();
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author is null or empty
+  if (!quote.author) {
+    authorText.textContent = "UnKnown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  //   Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  removeLoadingSpinner();
+}
+
+// Get Quotes From API
+async function getQuote() {
+  showLoadingSpinner();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Tweet Quotes
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listener
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load Page
+getQuote();
